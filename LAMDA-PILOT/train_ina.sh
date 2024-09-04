93f4623176b20fec4b8b34c96293cf181746139f
Geeks_Z <izhw1024@163.com>
2024-09-04 19:51:00 +0800

    新增实验脚本

--- LAMDA-PILOT/train_ina.sh
+++ LAMDA-PILOT/train_ina.sh
@@ -11,24 +11,6 @@ python main.py --config=./exps/simplecil_ina_B0_Inc10.json
 python main.py --config=./exps/simplecil_ina_B0_Inc20.json
 python main.py --config=./exps/simplecil_ina_B100_Inc5.json
 python main.py --config=./exps/simplecil_ina_B100_Inc10.json
-# icarl
-python main.py --config=./exps/icarl_ina_B0_Inc5.json
-python main.py --config=./exps/icarl_ina_B0_Inc10.json
-python main.py --config=./exps/icarl_ina_B0_Inc20.json
-python main.py --config=./exps/icarl_ina_B100_Inc5.json
-python main.py --config=./exps/icarl_ina_B100_Inc10.json
-# der
-python main.py --config=./exps/der_ina_B0_Inc5.json
-python main.py --config=./exps/der_ina_B0_Inc10.json
-python main.py --config=./exps/der_ina_B0_Inc20.json
-python main.py --config=./exps/der_ina_B100_Inc5.json
-python main.py --config=./exps/der_ina_B100_Inc10.json
-# foster
-python main.py --config=./exps/foster_ina_B0_Inc5.json
-python main.py --config=./exps/foster_ina_B0_Inc10.json
-python main.py --config=./exps/foster_ina_B0_Inc20.json
-python main.py --config=./exps/foster_ina_B100_Inc5.json
-python main.py --config=./exps/foster_ina_B100_Inc10.json
 # l2p
 python main.py --config=./exps/l2p_ina_B0_Inc5.json
 python main.py --config=./exps/l2p_ina_B0_Inc10.json
