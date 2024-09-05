1915c83de3dae1066695fb794a6c99fbb12e9228
Geeks_Z <izhw1024@163.com>
2024-09-05 17:22:24 +0800

    更新实验

--- LAMDA-PILOT/train_inr.sh
+++ LAMDA-PILOT/train_inr.sh
@@ -11,24 +11,6 @@ python main.py --config=./exps/simplecil_inr_B0_Inc10.json
 python main.py --config=./exps/simplecil_inr_B0_Inc20.json
 python main.py --config=./exps/simplecil_inr_B100_Inc5.json
 python main.py --config=./exps/simplecil_inr_B100_Inc10.json
-# icarl
-python main.py --config=./exps/icarl_inr_B0_Inc5.json
-python main.py --config=./exps/icarl_inr_B0_Inc10.json
-python main.py --config=./exps/icarl_inr_B0_Inc20.json
-python main.py --config=./exps/icarl_inr_B100_Inc5.json
-python main.py --config=./exps/icarl_inr_B100_Inc10.json
-# der
-python main.py --config=./exps/der_inr_B0_Inc5.json
-python main.py --config=./exps/der_inr_B0_Inc10.json
-python main.py --config=./exps/der_inr_B0_Inc20.json
-python main.py --config=./exps/der_inr_B100_Inc5.json
-python main.py --config=./exps/der_inr_B100_Inc10.json
-# foster
-python main.py --config=./exps/foster_inr_B0_Inc5.json
-python main.py --config=./exps/foster_inr_B0_Inc10.json
-python main.py --config=./exps/foster_inr_B0_Inc20.json
-python main.py --config=./exps/foster_inr_B100_Inc5.json
-python main.py --config=./exps/foster_inr_B100_Inc10.json
 # l2p
 python main.py --config=./exps/l2p_inr_B0_Inc5.json
 python main.py --config=./exps/l2p_inr_B0_Inc10.json
