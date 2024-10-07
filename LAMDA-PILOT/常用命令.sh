d6f69c68f3e8937cd213a90b6d99ea23fcf46427
Geeks_Z <izhw1024@163.com>
2024-10-08 07:42:12 +0800

    更新实验脚本

--- LAMDA-PILOT/常用命令.sh
+++ LAMDA-PILOT/常用命令.sh
@@ -10,9 +10,7 @@ sbatch train.slurm
 TODO
 nohup ./train_cifar.sh > ./log/2nd-cifar-stdout.log 2> ./log/2nd-cifar-stderr.log &
 nohup ./train_cifar.sh > ./log/3rd-cifar-stdout.log 2> ./log/3rd-cifar-stderr.log &
-nohup ./train_cub.sh > ./log/2nd-cub-stdout.log 2> ./log/2nd-cub-stderr.log &
 nohup ./train_cub.sh > ./log/3rd-cub-stdout.log 2> ./log/3rd-cub-stderr.log &
-nohup ./train_inr.sh > ./log/3rd-inr-stdout.log 2> ./log/3rd-inr-stderr.log &
 nohup ./train_omn.sh > ./res/3rd-omn.out 2>&1 &
 nohup ./train_omn.sh > ./log/2nd-omn-stdout.log 2> ./log/2nd-omn-stderr.log &
 nohup ./train_omn.sh > ./log/3rd-omn-stdout.log 2> ./log/3rd-omn-stderr.log &
