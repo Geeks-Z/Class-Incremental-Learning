e6aa58f4f43d4d089e293d0bbcdc1285c9becc55
Geeks_Z <izhw1024@163.com>
2024-11-14 21:29:10 +0800

    20241114

---
+++ LAMDA-PILOT/train_supp_omn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python main.py --config=./supp_exps/der_omn_B0_Inc30.json
+python main.py --config=./supp_exps/simplecil_omn_B0_Inc5.json
+
+
+
+
--- LAMDA-PILOT/常用命令.sh
+++ LAMDA-PILOT/常用命令.sh
@@ -8,7 +8,8 @@ cd Code/Research/CIL/LAMDA-PILOT/ &&
 sbatch train.slurm
 
 TODO
-nohup ./train_omn.sh > ./res/3rd-omn.out 2>&1 &
+nohup ./train_supp_omn.sh > ./log/3rd-supp-omn-stdout.log 2> ./log/3rd-supp-omn-stderr.log &
+nohup ./train_omn.sh > ./res/2nd-supp-omn.out 2>&1 &
 
 ----------------------------------------------------------------------------------------------------------
 #cifar
